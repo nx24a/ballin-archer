38a3d636728e0fdc202d7f78b6d436366152e758
maddoxx <maddoxx@192.168.115.100>
2014-04-12 21:13:04 +0200

    ADD building.js

---
+++ engine/logic/building.js
@@ -0,0 +1,23 @@
+var Building = (function() {
+    
+    //
+    var _type;
+    var _model;
+    var _icon;
+    var _inhabitants;
+    var _input;
+    var _output;
+    
+    var _production
+    
+    //
+    function Building(type) {
+        
+        
+    }
+    
+    //
+    Building.prototype.createOutput = function() {
+       this._production;  
+    };
+})();
