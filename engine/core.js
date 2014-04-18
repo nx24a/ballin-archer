3adfb925efa0f8ca9c0c141365e9442012ab8f84
maddoxx <maddoxx@192.168.115.100>
2014-04-18 23:12:59 +0200

    ADD alternative depth renderer ADD resources for testing

--- engine/core.js
+++ engine/core.js
@@ -4,9 +4,14 @@ var viewportControl = viewport.getContext("2d");
 viewport.width = 1900;
 viewport.height = 900;
 
-var offsetX = 450;
-var offsetY = 450;
+var offsetX = 200;
+var offsetY = 250;
+var viewportOffsetX = 550;
+var viewportOffsetY = 360;
 var timeoutvalue = 16;
+
+var buffer = [];
+
 //append viewport to document
 $("body").prepend("<div class=\"scrollUp\"><div>");
 $("body").prepend("<div class=\"scrollDown\"><div>");
@@ -21,6 +26,9 @@ $("body").prepend(viewport);
 $(document).ready(function(){
     init();
     
+    $("canvas").bind('mousemove', function(event) { 
+        $("#log").html("("+event.pageX+", "+event.pageY+")")
+    });
     
     $(".scrollUp").bind('mouseenter', function() {
         var self = $(this);
@@ -108,7 +116,7 @@ $(document).ready(function(){
     });
     
     function showOffSets() {
-        $("#log").html("("+offsetX+", "+offsetY+")");
+        //$("#log").html("("+offsetX+", "+offsetY+")");
     }
     
 });
@@ -127,6 +135,7 @@ window.requestAnimFrame = (function() {
 var timestampLast;
 
 function init() {
+    depthBuffer();
     timestampLast  = Date.now();
     main();
 }
@@ -141,6 +150,8 @@ function main() {
     
     //render 
     render();
+    //renderAlt();
+    //renderDepth();
     
     //update last timestamp
     timestampLast = currentTime;
@@ -174,27 +185,44 @@ function render() {
         loadImages(sources, function(images) {
             for(var y=0; y<maxDrawRangeY; y++) {  
                 for(var x=0; x<maxDrawRangeX; x++) {
-                    if(y%2 === 0) {
-                        posX = ((32*x)-16);
-
-                    } else {
-                        posX = (32*x);
-                    }
-                    if(map[startX+x][startY+y] == 0) {
-                        viewportControl.drawImage(images.water0, posX, posY+grid);  
-                    }
-                    if(map[startX+x][startY+y] == 1) {
-                        viewportControl.drawImage(images.water1, posX, posY+grid);
-                    }
-                    if(map[startX+x][startY+y] == 2) {
-                        viewportControl.drawImage(images.grass0, posX, posY+grid);
-                    }                   
-                } 
-
-                posY = posY+8;
-                if(gridcontrol) {
-                    grid = grid+1;
-                } 
+                    
+                    posX = 550+(x-y)*16;
+                    posY = (((x+y)*16)/2)-360;
+                    
+                        if(map[startX+x][startY+y][0] == 0) {
+                            viewportControl.drawImage(images.water0, posX, posY+grid);  
+                        }
+                        if(map[startX+x][startY+y][0] == 1) {
+                            viewportControl.drawImage(images.water1, posX, posY+grid);
+                        }
+                        if(map[startX+x][startY+y][0] == 2) {
+                            viewportControl.drawImage(images.grass0, posX, posY+grid);
+                        } 
+                        if(map[startX+x][startY+y][0] == 4) {
+                            viewportControl.drawImage(images.islandDefaultLeftFrontCorner, posX, posY+grid);
+                        } 
+                        if(map[startX+x][startY+y][0] == 5) {
+                            viewportControl.drawImage(images.islandDefaultRightBackCorner, posX, posY+grid+7);
+                        } 
+                        if(map[startX+x][startY+y][0] == 6) {
+                            viewportControl.drawImage(images.islandDefaultLeftBackCorner, posX, posY+grid+6);
+                        } 
+                        if(map[startX+x][startY+y][0] == 7) {
+                            viewportControl.drawImage(images.islandDefaultRightFrontCorner, posX, posY+grid);
+                        }
+                        if(map[startX+x][startY+y][0] == 8) {
+                            viewportControl.drawImage(images.bottomLeftCorner, posX, posY+grid+5);
+                        } 
+                        if(map[startX+x][startY+y][0] == 9) {
+                            viewportControl.drawImage(images.bottomRightCorner, posX, posY+grid);
+                        } 
+                        if(map[startX+x][startY+y][0] == 10) {
+                            viewportControl.drawImage(images.topLeftCorner, posX, posY+grid+10);
+                        } 
+                        if(map[startX+x][startY+y][0] == 11) {
+                            viewportControl.drawImage(images.topRightCorner, posX, posY+grid+6);
+                        }
+                }
             } 
           });
     
@@ -203,3 +231,95 @@ function render() {
 function animation(main) {
     
 }
+
+function depthBuffer() {
+    
+    var posX, posY;
+    
+    for(var x=0; x<1000; x++) {
+        for(var y=0; y<1000; y++) {
+
+
+
+            var tile = {
+              _positionX: x,
+              _positionY: y,
+              _texture: map[x][y][0]
+            };
+
+            var z = map[x][y][1];
+            
+            if(!buffer[z]) {
+                buffer[z] = [];
+            }
+            
+            buffer[z].push(tile);
+        }        
+    } 
+}
+
+function renderDepth() {
+        
+        var defaultTileWidth = 16;
+        var defaultTileHight = 8;
+        
+        var maxDrawRangeX = Math.ceil(viewport.width/defaultTileWidth);
+        var maxDrawRangeY = Math.ceil(viewport.height/defaultTileHight);
+        var startX = offsetX-Math.ceil(maxDrawRangeX/2);
+        var startY = offsetY-Math.ceil(maxDrawRangeY/2);
+    
+        //default
+        var grid = 0;
+        var gridcontrol = false;
+        
+        var cameraFocusedTileX = (offsetX-offsetY)*16;
+        var cameraFocusedTileY = ((offsetX+offsetY)*16)/2;
+
+        var diffY = 2000;
+        var diffX = 2200;
+        
+        var posX, posY;
+        
+        loadImages(sources, function(images) {
+            
+            for(var z=0; z<buffer.length;z++) {
+                for(var o=0; o<buffer[z].length; o++) {
+                    
+                    var obj = buffer[z][o];
+                    
+                    if(obj._positionX > (startX-maxDrawRangeX) && obj._positionX < (startX+maxDrawRangeX)) {
+                        if(obj._positionY > (startY-maxDrawRangeY) && obj._positionY < (startY+maxDrawRangeY)) {
+                        
+                            posX = (viewportOffsetX+(obj._positionX-obj._positionY)*16)+startX;
+                            posY = ((((obj._positionX+obj._positionY)*16)/2)-viewportOffsetY)+startY;
+                        
+                            if(obj._texture == 0) {
+                                viewportControl.drawImage(images.water0, posX, posY);  
+                            }
+                            if(obj._texture == 1) {
+                                viewportControl.drawImage(images.water1, posX, posY);  
+                            }
+                            if(obj._texture == 2) {
+                                viewportControl.drawImage(images.grass0, posX, posY);  
+                            }
+                            if(obj._texture == 4) {
+                                viewportControl.drawImage(images.islandDefaultLeftFrontCorner, posX, posY);  
+                            }
+                            if(obj._texture == 5) {
+                                viewportControl.drawImage(images.islandDefaultRightBackCorner, posX, posY);  
+                            }
+                            if(obj._texture == 6) {
+                                viewportControl.drawImage(images.islandDefaultLeftBackCorner, posX, posY);  
+                            }
+                            if(obj._texture == 7) {
+                                viewportControl.drawImage(images.islandDefaultRightFrontCorner, posX, posY);  
+                            }  
+                            
+                        }
+                        
+                    }
+                }
+            }
+        });
+        
+  }
--- engine/game/map/map.js
+++ engine/game/map/map.js
@@ -6,34 +6,74 @@ for(var i=0; i<map_tiles_x; i++) {
     if(!map[i]) {
         map[i] = [];
     }
+    
+    for(var j=0; j<map_tiles_y; j++) {
+        if(!map[i][j]) {
+            map[i][j] = [];
+        }
+    }
+    
 }
 
 for(var i=0; i<map_tiles_x; i++){
     for(var j=0; j<map_tiles_y; j++){
-        map[i][j] = "1";
-    }   
-}
-
-var offsetIslandX = 480;
-var offsetIslandY = 450;
-
-var offsetIslandXStart = Math.floor(offsetIslandX/2);
-var maxMapI = 20;
-for(var i=0; i<maxMapI+1; i++){  
-    for(var j=0; j<((2*i)+1); j++){
-        map[(offsetIslandX-i)+j][offsetIslandY] = "2";
-        map[(offsetIslandX-i)+j][(offsetIslandX+(Math.floor(maxMapI/2)))-i] = "2";
+        map[i][j][0] = 1;
+        map[i][j][1] = 0;
     }   
     offsetIslandY++;
 }
 
 
+island(200,200);
+island(260,300);
+
+function island(x,y) {
+for(var i=x; i<x+20; i++){
+    for(var j=y; j<y+20; j++){
+            map[i][j][0] = 2;
+            map[i][j][1] = 2;
+        if(j==y+19) {
+            map[i][j][0] = 4;
+            map[i][j][1] = 2;
+        }
+        if(j==y) {
+            map[i][j][0] = 5;
+            map[i][j][1] = 1;
+        }
+        if(i==x) {
+            map[i][j][0] = 6;
+            map[i][j][1] = 1;
+        }
+        if(i==x+19) {
+            map[i][j][0] = 7;
+            map[i][j][1] = 2;
+        }
+        
+
+    }
+    
+
+map[x][y][0] = 10;
+map[x+19][y][0] = 11;
+map[x][y+19][0] = 8;
+map[y+19][y+19][0] = 9;
+    
+}
+
+}
 //island size
 
-
 var sources = {
-  water0: 'water-0.png',  
-  water1: 'water-1.png',
-  grass0: 'grass-0.png'
+  water0: 'resources/test/water-0.png',  
+  water1: 'resources/test/water-1.png',
+  grass0: 'resources/test/grass-0.png',
+  islandDefaultLeftFrontCorner: 'resources/test/cornerFrontLeft.png',
+  islandDefaultLeftBackCorner: 'resources/test/cornerBackLeft.png',
+  islandDefaultRightFrontCorner: 'resources/test/cornerFrontRight.png',
+  islandDefaultRightBackCorner: 'resources/test/cornerBackRight.png',
+  bottomLeftCorner: 'resources/test/bottomLeftCorner.png',
+  bottomRightCorner: 'resources/test/bottomRightCorner.png',
+  topLeftCorner: 'resources/test/topLeftCorner.png',
+  topRightCorner: 'resources/test/topRightCorner.png'
 };
 
